21adcc7dda249608ff58f26953f905471d0ad368
Domingo Lopez Rodríguez <33377919+neuroimaginador@users.noreply.github.com>
2025-01-20 16:51:11 +0100

    Some changes

--- scripts/manage_publications.R
+++ scripts/manage_publications.R
@@ -91,28 +91,35 @@ plot_citation_history <- function(df) {
     plotly::config(displayModeBar = FALSE)
 }
 
-get_citations_from_doi <- function(doi) {
-  work <- openalexR::oa_fetch(
-    entity = "works",
-    doi = doi,
-    verbose = FALSE
+source(
+  here::here(
+    "scripts",
+    "citations_openalex.R"
   )
+)
 
-  by_year <- work$counts_by_year
-  url <- work$cited_by_api_url
-  json <- jsonlite::read_json(url)
+# get_citations_from_doi <- function(doi) {
+#   work <- openalexR::oa_fetch(
+#     entity = "works",
+#     doi = doi,
+#     verbose = FALSE
+#   )
 
-  dois <- json$results |> sapply(\(x) x$doi)
-  years <- json$results |> sapply(\(x) x$publication_year)
-  o <- order(years, decreasing = TRUE)
-  bibs <- RefManageR::GetBibEntryWithDOI(dois[o])
-  RefManageR::NoCite(bibs)
-  txt <- bibs |>
-    RefManageR::PrintBibliography(.opts = list(
-      bib.style = "numeric",
-      style = "markdown"
-    )) |>
-    capture.output()
+#   by_year <- work$counts_by_year
+#   url <- work$cited_by_api_url
+#   json <- jsonlite::read_json(url)
 
-  return(list(by_year = by_year, citations = txt))
-}
+#   dois <- json$results |> sapply(\(x) x$doi)
+#   years <- json$results |> sapply(\(x) x$publication_year)
+#   o <- order(years, decreasing = TRUE)
+#   bibs <- RefManageR::GetBibEntryWithDOI(dois[o])
+#   RefManageR::NoCite(bibs)
+#   txt <- bibs |>
+#     RefManageR::PrintBibliography(.opts = list(
+#       bib.style = "numeric",
+#       style = "markdown"
+#     )) |>
+#     capture.output()
+
+#   return(list(by_year = by_year, citations = txt))
+# }
